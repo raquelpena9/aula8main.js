1e4468caa821e1929aa6bf507a8ed3e466d08ec9
Raquel Pena <139807495+raquelpena9@users.noreply.github.com>
2023-08-08 15:26:31 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const caixa de som = {marca: "JBL",
+modelo: "Boombox 2 X2BLKBR",
+ano: "2023",
+cor: "preto",
+tipo: "com alça",
+volume: function () {alert('Loucuraaaaa')}
+};
+console.log(volume.caixadesom())
